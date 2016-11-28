16f837897affc10671c86354ac97fcd586098e57
cs350ostbym <megan.ostby@eagles.ewu.edu>
2016-11-28 11:40:05 -0800

    Added information to the Airport Class

--- DPP/src/airtravel/Airport.java
+++ DPP/src/airtravel/Airport.java
@@ -5,15 +5,25 @@ package airtravel;
 //TODO: Megan
 public class Airport
 {
-    //Test
     String name;
 
-    //Objects from this class represent airports
-    //Only the name needs to be maintained
+    public Airport(String name) {
+        try {
+            if (name.length() > 3 || name.length() < 3)
+            {
+                //Must be alphabtic characters
+                throw new IllegalArgumentException("Airport name must be exactly 3 characters long!");
+            }
+
+            this.name = name;
+        } catch (IllegalArgumentException e) {
+            System.out.println("This airport was not created due a size issue with the name.");
+        }
+    }
 
     public String toSting()
     {
-        String airport = "";
+        String airport = name;
 
         return airport;
     }
