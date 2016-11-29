2a69a020085e7e574063b36c3aba84f29909cb3c
sagather <samantha.agather@hotmail.com>
2016-11-29 12:36:58 -0800

    copied all over

--- DPP/src/airtravel/Airline.java
+++ DPP/src/airtravel/Airline.java
@@ -5,26 +5,14 @@ import java.util.HashMap;
 /**
  * Created by Megan Ostby on 11/22/2016.
  */
-//TODO: Megan
 public class Airline
 {
-    private HashMap<String,Flight> flights;
     private String name;
 
     public Airline(String name)
     {
         this.name = name;
     }
-
-    //public addFlight(Flight flight)
-    //{
-
-    //}
-
-    //Maintains information about airlines
-    //Airlines can have 0 or more flights associated with it
-    //When created no flights are associated with an airline
-    //Each flight (From flight class?) must have a unique ID for that particular airline
 
     public String toString()
     {
